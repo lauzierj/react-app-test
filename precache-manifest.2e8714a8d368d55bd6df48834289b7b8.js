894cba82740dd83e99d0198e9a2babd674c0c64a
Jacob Lauzier <jacob.lauzier@machinemetrics.com>
2020-04-22 16:05:22 -0400

    initial commit

---
+++ precache-manifest.2e8714a8d368d55bd6df48834289b7b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f97d93efadf6e2944077269a8b2aa63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c67eae7bea84fe606b0c",
+    "url": "/static/css/2.f22687d4.chunk.css"
+  },
+  {
+    "revision": "52fa26f812e8cb452ea0",
+    "url": "/static/css/main.7dc2c717.chunk.css"
+  },
+  {
+    "revision": "c67eae7bea84fe606b0c",
+    "url": "/static/js/2.79754b0a.chunk.js"
+  },
+  {
+    "revision": "111d9dd0d4f1e21fb19389ec92e1fa72",
+    "url": "/static/js/2.79754b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fa26f812e8cb452ea0",
+    "url": "/static/js/main.9937036a.chunk.js"
+  },
+  {
+    "revision": "83a0bae63143e1d95f58",
+    "url": "/static/js/runtime-main.248d202e.js"
+  }
+]);
