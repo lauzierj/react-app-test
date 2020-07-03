18a758f4ee5e91f9941c8458cc4ea39c52ea4f9f
Jacob Lauzier <jacob.lauzier@machinemetrics.com>
2020-07-03 09:37:39 -0400

    build for github

---
+++ precache-manifest.22dfdb87d05d563abb2bea65268055d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143c747f04bed41ccb27ed68f7f6a5e5",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "55c5d9bd80b50c374ca9",
+    "url": "/react-app-test/static/css/2.f22687d4.chunk.css"
+  },
+  {
+    "revision": "0586bf6def78230bcb07",
+    "url": "/react-app-test/static/css/main.4e530852.chunk.css"
+  },
+  {
+    "revision": "55c5d9bd80b50c374ca9",
+    "url": "/react-app-test/static/js/2.4002d4cb.chunk.js"
+  },
+  {
+    "revision": "4b454a4286827ff3a5569f8875abd063",
+    "url": "/react-app-test/static/js/2.4002d4cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0586bf6def78230bcb07",
+    "url": "/react-app-test/static/js/main.6dd5a3e2.chunk.js"
+  },
+  {
+    "revision": "6bd3a39f2c4e90178735",
+    "url": "/react-app-test/static/js/runtime-main.f2a3c9a1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9eae1b031260c2401b80c5ecb12a3733.js"
+  "/react-app-test/precache-manifest.22dfdb87d05d563abb2bea65268055d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
